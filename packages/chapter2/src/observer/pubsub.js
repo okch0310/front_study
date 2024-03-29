e74126777a03a7b4eaabbccc62294dfaac9ae50c
채현 <79138338+okch0310@users.noreply.github.com>
2024-03-29 10:02:02 +0900

    STEP5 과제E_옥채현

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,31 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentCallback = null;
+let listeners = {};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  currentCallback = fn;
+  fn();
+  currentCallback = null;
+};
+
+export const 발행기관 = (obj) => {
+  const newObject = new Proxy(obj, {
+    set(target, prop, value) {
+      target[prop] = value;
+      if (listeners[prop]) {
+        listeners[prop].forEach((cb) => cb());
+      }
+      return true;
+    },
+    get(target, prop) {
+      if (currentCallback && typeof currentCallback === "function") {
+        if (!listeners[prop]) {
+          listeners[prop] = new Set();
+        }
+        listeners[prop].add(currentCallback);
+      }
+      return target[prop];
+    },
+  });
+
+  return newObject;
+};
